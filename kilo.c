eb28b87df4912c3f51493303b9f5fc986305c1f6
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2017-09-04 22:19:48 +0100

    Initial commit
     Changes to be committed:
    	new file:   Makefile
    	new file:   kilo
    	new file:   kilo.c

---
+++ kilo.c
@@ -0,0 +1,40 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+
+struct termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+void enableRawMode() {
+
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
+	raw.c_iflag &= ~(IXON); //Disables ctrl-s and ctrl-q
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //Disables echo, concanical mode, ctrl-v, ctrl-c, ctrl-z signals
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+
+	enableRawMode();
+
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+		if (iscntrl(c)) {
+			printf("%d\n", c);
+		} else {
+			printf("%d (%c)\n", c, c );
+		}
+	}
+
+    return 0;
+}
