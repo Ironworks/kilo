427f6cea7eb184da2c448047abb33016e72e3bb6
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2017-09-04 22:53:43 +0100

    add timeout for read

--- kilo.c
+++ kilo.c
@@ -20,6 +20,8 @@ void enableRawMode() {
 	raw.c_oflag &= ~(OPOST); //Disables all output processing
 	raw.c_cflag &= ~(CS8); //Set character size to 8 bits
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); //Disables echo, concanical mode, ctrl-v, ctrl-c, ctrl-z signals
+	raw.c_cc[VMIN] = 0; //Min number of bytes of input required before read() returns
+	raw.c_cc[VTIME] = 0; //Max time to wait before read() returns
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -28,14 +30,18 @@ int main() {
 
 	enableRawMode();
 
-	char c;
-	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+	while (1)  {
+
+		char c = '\0';
+		read(STDIN_FILENO, &c, 1);
 
 		if (iscntrl(c)) {
 			printf("%d\r\n", c);
 		} else {
 			printf("%d (%c)\r\n", c, c );
 		}
+
+		if (c == 'q') break;
 	}
 
     return 0;
